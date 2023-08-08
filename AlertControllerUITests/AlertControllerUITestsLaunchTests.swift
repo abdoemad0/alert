b825ce925455d20585b213fba6066765e16c8068
abdo <emad>
2023-08-08 14:07:29 +0300

    Initial Commit

---
+++ AlertControllerUITests/AlertControllerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AlertControllerUITestsLaunchTests.swift
+//  AlertControllerUITests
+//
+//  Created by abdo emad  on 08/08/2023.
+//
+
+import XCTest
+
+final class AlertControllerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
